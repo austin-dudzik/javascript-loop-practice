018bb529c1dfd65eb1862450f85a8b045a2262c7
Austin Dudzik <austin.dudzik@gmail.com>
2021-02-16 09:40:10 -0600

    Initial commit

--- images.js
+++ images.js
@@ -1,64 +1,81 @@
-$(document).ready(function () {
-    $("button").click(runLoops);
+$(document).ready(function() {
+  $("button").click(runLoops);
 });
 
 function runLoops() {
-    // Clear out all existing images from divs
-    $("div.imageHolder").empty();
+  // Clear out all existing images from divs
+  $("div.imageHolder").empty();
 
-    // Get the number from the box
-    let num = parseInt($("input#numPics").val());
+  // Get the number from the box
+  let num = parseInt($("input#numPics").val());
 
-    // Print the desired number of images for each loop
-    $("span#while-num").text(num + 1);
-    $("span#do-while-num").text(num - 1);
-    $("span#for-num").text(num);
-
-
-    /*
-     Create a while loop that runs "num + 1" times.
-
-     The body of the loop should contain the statement:
-
-        addImageToDiv("#while-loop");
-    */
+  // Print the desired number of images for each loop
+  $("span#while-num").text(num + 1);
+  $("span#do-while-num").text(num - 1);
+  $("span#for-num").text(num);
 
 
+  /*
+   Create a while loop that runs "num + 1" times.
+
+   The body of the loop should contain the statement:
+
+      addImageToDiv("#while-loop");
+  */
+
+  let numNeeded = num + 1;
+
+  let numCreated = 0;
+
+  while (numCreated < numNeeded) {
+    addImageToDiv("#while-loop");
+    numCreated++;
+  }
 
 
+  /*
+   Create a do-while loop that runs "num - 1" times.
 
-    /*
-     Create a do-while loop that runs "num - 1" times.
+   The body of the loop should contain the statement:
 
-     The body of the loop should contain the statement:
+      addImageToDiv("#do-while-loop");
+  */
 
-        addImageToDiv("#do-while-loop");
-    */
+  let doNumNeeded = num - 1;
 
+  let doNumCreated = 0;
 
-
-
-
-    /*
-     Create a for loop that runs exactly "num" times.
-
-     The body of the loop should contain the statement:
-
-        addImageToDiv("#for-loop");
-    */
+  do {
+    addImageToDiv("#do-while-loop");
+    doNumCreated++;
+  } while (doNumCreated < doNumNeeded);
 
 
+  /*
+   Create a for loop that runs exactly "num" times.
 
+   The body of the loop should contain the statement:
+
+      addImageToDiv("#for-loop");
+  */
+
+  let forNumCreated = 0;
+
+  for (forNumCreated = 0; forNumCreated < num; forNumCreated++) {
+
+    addImageToDiv("#for-loop");
+
+  }
 
 
 }
 
 function addImageToDiv(divId) {
-    let fileName = $(":radio[name='pictureSelect']:checked").val();
-    // Create a new image element
-    let imageElement = $("<img>");
-    // Set its source attribute
-    imageElement.attr("src", `img/${fileName}`);
-    // Add it as a child of another element
-    $(divId).append(imageElement);
+  let fileName = $(":radio[name='pictureSelect']:checked").val();
+  // Create a new image element
+  let imageElement = $("<img>");
+  // Set its source attribute
+  imageElement.attr("src", `img/${fileName}`);
+  // Add it as a child of another element
+  $(divId).append(imageElement);
 }
--- text.js
+++ text.js
@@ -1,83 +1,113 @@
-$(document).ready(function () {
-    $("form#loopTestForm").submit(runLoops);
+$(document).ready(function() {
+  $("form#loopTestForm").submit(runLoops);
 });
 
 function runLoops(event) {
-    // Stop the form from submitting
-    event.preventDefault();
+  // Stop the form from submitting
+  event.preventDefault();
 
-    // This variable will contain one of three strings:
-    // "for-loop", "while-loop", or "do-while-loop"
-    let selectedLoop = $("input[name=loop-type]:checked").val();
+  // This variable will contain one of three strings:
+  // "for-loop", "while-loop", or "do-while-loop"
+  let selectedLoop = $("input[name=loop-type]:checked").val();
 
-    /*
-        Depending on which loop was selected,
-        call the appropriate function.
+  /*
+      Depending on which loop was selected,
+      call the appropriate function.
 
-        Possible values of selectedLoop and
-        the corresponding function calls are:
+      Possible values of selectedLoop and
+      the corresponding function calls are:
 
-        "for-loop"	    ==>  runForLoop()
-        "while-loop"    ==>  runWhileLoop()
-        "do-while-loop" ==>  runDoWhileLoop()
-    */
+      "for-loop"	    ==>  runForLoop()
+      "while-loop"    ==>  runWhileLoop()
+      "do-while-loop" ==>  runDoWhileLoop()
+  */
 
+  switch (selectedLoop) {
 
+    case "for-loop":
+      runForLoop();
+      break;
 
+    case "while-loop":
+      runWhileLoop();
+      break;
+
+    case "do-while-loop":
+      runDoWhileLoop();
+      break;
+
+  }
 
 
 }
 
 
 function runWhileLoop() {
-    let number = parseInt($("input#myNumber").val());
-    let output = "";
+  let number = parseInt($("input#myNumber").val());
+  let output = "A";
 
-    /*
-        Use a while loop to output the letter A
-        "number" times. For example, if "number" is 5,
-        then output should be "AAAAA".
+  /*
+      Use a while loop to output the letter A
+      "number" times. For example, if "number" is 5,
+      then output should be "AAAAA".
 
-        (Or for a challenge, output the first "number" letters of the alphabet!)
+      (Or for a challenge, output the first "number" letters of the alphabet!)
 
-        Hint: Use jQuery's append() to add text to an element
-        (instead of replacing text)
+      Hint: Use jQuery's append() to add text to an element
+      (instead of replacing text)
 
-        $("div#while-result").append(...);
-    */
+      $("div#while-result").append(...);
+  */
 
+  let numPrinted = 0;
 
-    $("div#while-result").text(output);
+  while (numPrinted < number) {
+    $("div#while-result").append(output);
+    numPrinted++;
+  }
+
 }
 
 
 function runForLoop() {
-    let number = parseInt($("input#myNumber").val());
-    let sum = 0;
+  let number = parseInt($("input#myNumber").val());
+  let sum = 0;
 
-    /*
-        Use a for loop to add the numbers 1 through "number"
-        into the variable "sum". Output the sum to the div.
-    */
+  /*
+      Use a for loop to add the numbers 1 through "number"
+      into the variable "sum". Output the sum to the div.
+  */
 
-    $("div#for-result").text(sum);
+  let numPrinted = 0;
+
+  for (numPrinted = 0; numPrinted < number; numPrinted++) {
+    sum += numPrinted + 1;
+  }
+
+  $("div#for-result").text(sum);
 }
 
 
 function runDoWhileLoop() {
-    let number = parseInt($("input#myNumber").val());
+  let number = parseInt($("input#myNumber").val());
 
-    /*
-        Use a do-while loop to output the numbers 1 through
-        "number". For example, if "number" is 5, then output
-        should be "12345".
+  /*
+      Use a do-while loop to output the numbers 1 through
+      "number". For example, if "number" is 5, then output
+      should be "12345".
 
-        Hint: Use jQuery's append() to add text to an element
-        (instead of replacing text)
+      Hint: Use jQuery's append() to add text to an element
+      (instead of replacing text)
 
-        $("div#do-while-result").append(...);
-    */
+      $("div#do-while-result").append(...);
+  */
 
+  numPrinted = 0;
+
+  do {
+    $("div#do-while-result").append(numPrinted + 1);
+    numPrinted++;
+  } while (number > numPrinted);
 
 
 }
